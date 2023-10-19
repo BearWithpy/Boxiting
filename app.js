0e7b236ad903e3ecb37e8651c53fdc9c6c6782c5
Junsu Park <pksjmh5295@gmail.com>
2023-10-19 12:20:49 +0900

    wrong commit

---
+++ app.js
@@ -0,0 +1,10 @@
+{
+    {
+        {
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index]
+                // 잘못된 커밋
+            }
+        }
+    }
+}
